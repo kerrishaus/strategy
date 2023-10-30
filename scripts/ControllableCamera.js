5b71149b07c8ff5de8b85f4c195a8871cd7912e3
Kenny McCormick <kennyrmccormick+github@gmail.com>
2023-10-29 20:34:29 -0500

    work on controllable camera

--- scripts/ControllableCamera.js
+++ scripts/ControllableCamera.js
@@ -5,5 +5,54 @@ export class ControllableCamera extends THREE.PerspectiveCamera
     constructor()
     {
         super(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+        this.element = null;
+    }
+
+    attach(element)
+    {
+        this.element = element;
+
+        this.element.addEventListener("keydown",    this.onKeyDown);
+        this.element.addEventListener("keyup",      this.onKeyUp);
+        this.element.addEventListener("mousedown",  this.onMouseDown);
+        this.element.addEventListener("mouseup",    this.onMouseUp);
+        this.element.addEventListener("mousemove",  this.onMouseMove);
+        this.element.addEventListener("mousewheel", this.onMouseWheel);
+    }
+
+    detatch(element)
+    {
+
+    }
+
+    onKeyDown(event)
+    {
+
+    }
+
+    onKeyUp(event)
+    {
+
+    }
+
+    onMouseUp(event)
+    {
+        
+    }
+    
+    onMouseDown(event)
+    {
+        
+    }
+    
+    onMouseMove(event)
+    {
+        
+    }
+
+    onMouseWheel(event)
+    {
+
     }
 }
